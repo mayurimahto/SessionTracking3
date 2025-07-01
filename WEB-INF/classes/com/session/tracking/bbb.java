7033566dc9f045de78e3c77386af43e345ec5712
Unstop GitHub Commands <mahtomayuri@gmail.com>
2025-07-01 15:45:27 +0530

    classes

---
+++ WEB-INF/classes/com/session/tracking/bbb.java
@@ -0,0 +1,43 @@
+package com.session.tracking;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+import java.net.*;
+
+public class bbb extends HttpServlet
+{
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	{
+		try
+		{
+			String name=request.getParameter("name");
+			String city=request.getParameter("city");
+			System.out.println("Data Arrived");
+			System.out.println("Name : "+name);
+			System.out.println("City : "+city);
+			PrintWriter pw=response.getWriter();
+			response.setContentType("text/html");
+
+			pw.println("<!DOCTYPE HTML>");
+			pw.println("<html lang='en'>");
+			pw.println("<head>");
+			pw.println("<meta charset='utf-8'>");
+			pw.println("<title>Session Tracking using URL Rewriting</title>");
+			pw.println("</head>");
+			pw.println("<body>");
+			pw.println("Data Saved<br>");
+			pw.println("Name : "+name+"<br>");
+			pw.println("City : "+city+"<br>");
+			
+			pw.println("<a href='/SessionTracking2/ccc?name="+URLEncoder.encode(name)+"&city="+URLEncoder.encode(city)+"'>Save</a>");
+
+			pw.println("</body>");
+			pw.println("</html>");
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
---
+++ WEB-INF/classes/com/session/tracking/ccc.java
@@ -0,0 +1,42 @@
+package com.session.tracking;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+
+public class ccc extends HttpServlet
+{
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	{
+		try
+		{
+			String name=request.getParameter("name");
+			String city=request.getParameter("city");
+			System.out.println("Data Arrived");
+			System.out.println("Name : "+name);
+			System.out.println("City : "+city);
+			PrintWriter pw=response.getWriter();
+			response.setContentType("text/html");
+
+			pw.println("<!DOCTYPE HTML>");
+			pw.println("<html lang='en'>");
+			pw.println("<head>");
+			pw.println("<meta charset='utf-8'>");
+			pw.println("<title>Session Tracking using URL Rewriting</title>");
+			pw.println("</head>");
+			pw.println("<body>");
+			pw.println("Data Saved<br>");
+			pw.println("Name : "+name+"<br>");
+			pw.println("City : "+city+"<br>");
+			pw.println("<form action='/SessionTracking2/index.html'>");
+			pw.println("<button type='submit'>Ok</button>");
+			pw.println("</form>");
+			pw.println("</body>");
+			pw.println("</html>");
+			
+		}
+		catch(Exception e)
+		{
+			System.out.println(e);
+		}
+	}
+}
